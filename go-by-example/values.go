aeca4689be771ed0d8a24bf632088b58a27bc28d
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-06-20 08:58:32 +0530

    switch

--- go-by-example/values.go
+++ go-by-example/values.go
@@ -1,60 +1,60 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"math"
-// )
+import (
+	"fmt"
+	"math"
+)
 
-// func main() {
-// 	fmt.Println(true && true)   // true
-// 	fmt.Println(false && true)  // false
-// 	fmt.Println(true && false)  // false
-// 	fmt.Println(false && false) // false
+func main() {
+	fmt.Println(true && true)   // true
+	fmt.Println(false && true)  // false
+	fmt.Println(true && false)  // false
+	fmt.Println(false && false) // false
 
-// 	fmt.Println(true || true)              // true
-// 	fmt.Println(false || true)             // true
-// 	fmt.Println(true || false)             // true
-// 	fmt.Println(false || false)            // false
-// 	fmt.Println("Exclamation: ", !!!false) // true
-// 	variables()
+	fmt.Println(true || true)              // true
+	fmt.Println(false || true)             // true
+	fmt.Println(true || false)             // true
+	fmt.Println(false || false)            // false
+	fmt.Println("Exclamation: ", !!!false) // true
+	variables()
 
-// }
+}
 
-// func variables() {
-// 	var names = "ray"
-// 	name := "Sam" // short assignment , not global
+func variables() {
+	var names = "ray"
+	name := "Sam" // short assignment , not global
 
-// 	var a, b, c int = 1, 2, 3
+	var a, b, c int = 1, 2, 3
 
-// 	status := 100
-// 	one := string(status)
+	status := 100
+	one := string(status)
 
-// 	fmt.Println(name, names, a, b, c, status, one)
+	fmt.Println(name, names, a, b, c, status, one)
 
-// 	my_num := 10
-// 	new_num := float64(my_num)
-// 	println("new", new_num)
+	my_num := 10
+	new_num := float64(my_num)
+	println("new", new_num)
 
-// 	age := 20
-// 	_ = age
+	age := 20
+	_ = age
 
-// 	constants()
+	constants()
 
-// }
+}
 
-// func constants() {
-// 	const name string = "sam"
-// 	const age = 10
+func constants() {
+	const name string = "sam"
+	const age = 10
 
-// 	fmt.Println(math.Sin(age))
-// 	const n = 500000000
-// 	const d = 3e20 / n // 30000000.../50000000  => 300000000000
-// 	fmt.Println(d)
-// 	fmt.Println(int64(d))
+	fmt.Println(math.Sin(age))
+	const n = 500000000
+	const d = 3e20 / n // 30000000.../50000000  => 300000000000
+	fmt.Println(d)
+	fmt.Println(int64(d))
 
-// 	var a = 500000000
-// 	var b  = 3e20 / a // 30000000.../50000000  => 300000000000
-// 	fmt.Println(b)
-// 	fmt.Println(int64(b))
+	var a = 500000000
+	var b = 3e20 / a // 30000000.../50000000  => 300000000000
+	fmt.Println(b)
+	fmt.Println(int64(b))
 
-// }
+}
